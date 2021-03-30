77ff3d6af9fcd32c6b4418cf556ee445cca07b9b
Rodolfo <rsfeitosa88@gmail.com>
2021-03-29 23:13:09 -0300

    Inclused Login Screen

--- src/navigation/Stack.js
+++ src/navigation/Stack.js
@@ -6,7 +6,7 @@ import Login from '../views/Login'
 const Stack = createStackNavigator()
 
 export default props =>(
-    <Stack.Navigator initialRouteName="Start">
+    <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Start" 
             options={{headerShown: false}}  component={Start} />
         <Stack.Screen name="Login" 
