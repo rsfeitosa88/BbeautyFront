e7dbf5acdb4a3ed30ef05106c862c6d8e58c5e3c
Rodolfo Feitosa <rsfeitosa88@gmail.com>
2021-04-03 01:40:43 -0300

    Styles done

---
+++ src/assets/styles/startStyle.js
@@ -0,0 +1,52 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  image: {
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center'
+  },
+  containerBb: {
+    flex: 3,
+    left: 25
+  },
+  backgroundText: {
+    backgroundColor: 'rgba(255, 255, 255, 0.50)',
+    width: 279,
+    height: 87,
+    top: 140,
+    borderBottomLeftRadius: 30
+  },
+  textBb: {
+    fontFamily: 'Poppins-Bold',
+    fontSize: 60,
+    lineHeight: 90,
+    color: '#8E3385',
+    textAlign: 'center'
+    },
+  containerText: {
+    flex: 1,
+    width: 250,
+    left: 25,
+  },
+  textoInicial: {
+    color: 'white',
+    fontFamily: 'Poppins-Bold',
+    fontSize: 25
+  },
+  containerButton: {
+    flex: 0.7,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '100%',
+    height: 60,
+  }
+})
+
+export default styles
