ab6f6fb7dd5cc3c33c7dbe9dd460294d876fae17
Rodolfo Feitosa <rsfeitosa88@gmail.com>
2021-04-03 02:20:01 -0300

    Code format fixed

--- src/navigation/Stack.js
+++ src/navigation/Stack.js
@@ -1,21 +1,31 @@
-import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
-import Start from '../views/Start';
-import Login from '../views/Login';
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import Start from '../views/Start'
+import Login from '../views/Login'
+import PreLoading from '../views/PreLoading'
+import SignUp from '../views/SignUp'
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 export default props => (
+  
   <Stack.Navigator initialRouteName="Login">
     <Stack.Screen
+      name="PreLoading"
+      options={{ headerShown: false }}
+      component={PreLoading}
+    />
+    <Stack.Screen
       name="Start"
-      options={{headerShown: false}}
+      options={{ headerShown: false }}
       component={Start}
     />
     <Stack.Screen
       name="Login"
-      options={{headerShown: false}}
+      options={{ headerShown: false }}
       component={Login}
     />
+    
   </Stack.Navigator>
+
 );
