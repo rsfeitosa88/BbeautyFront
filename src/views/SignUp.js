5a4123aef3571da6083b2ff52c7a959e5ed14781
Rodolfo Feitosa <rsfeitosa88@gmail.com>
2021-04-04 19:07:57 -0300

    Next step - Implement SignUp

---
+++ src/views/SignUp.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Text, { View } from 'react-native'
+
+const SignUp = () => {
+
+    return (
+        <View>
+            <Text>
+                Teste
+            </Text>
+        </View>
+    )
+}
+
+export default SignUp
