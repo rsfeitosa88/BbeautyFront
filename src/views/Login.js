bcb58f0476609314561eb2656ec489a930f0b474
Rodolfo Feitosa <rsfeitosa88@gmail.com>
2021-04-04 19:04:44 -0300

    Updated, only codes in archive and the style is imported

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,5 +1,13 @@
 import React from 'react'
-import { View, Text, ImageBackground, Alert, KeyboardAvoidingView } from 'react-native'
+import { 
+  View, 
+  Text, 
+  ImageBackground, 
+  Alert, 
+  KeyboardAvoidingView, 
+  Keyboard, 
+  TouchableWithoutFeedback 
+} from 'react-native'
 
 import styles from '../assets/styles/loginStyle'
 import Button from '../components/ButtonFactory'
@@ -15,81 +23,83 @@ export default props => (
       keyboardVerticalOffset={Platform.OS == "ios" ? 0 : 20}
       enabled={Platform.OS === "ios" ? true : false}
     >
-      <ImageBackground
-        source={require(image)}
-        style={styles.image}
-        blurRadius={50}
-      >
-        <View style={styles.containerHead}>
-          <Text style={styles.headTextBold}>Seja bem-vinde</Text>
-          <Text style={styles.headTextNormal}>de volta!</Text>
-        </View>
-        <View style={styles.bodyContainer}>
-          <View style={styles.fieldContainer}>
-            <TextInput fieldName="E-mail" placeHolder="Digite seu e-mail" />
-            <TextInput fieldName="Senha" placeHolder="Digite sua senha" />
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ImageBackground
+          source={require(image)}
+          style={styles.image}
+          blurRadius={50}
+        >
+          <View style={styles.containerHead}>
+            <Text style={styles.headTextBold}>Seja bem-vinde</Text>
+            <Text style={styles.headTextNormal}>de volta!</Text>
           </View>
-          <View style={styles.linksContainer}>
-            <Button
-              text="Esqueceu a senha?"
-              color="transparent"
-              width={180}
-              height={26}
-              colorText="#FFFFFF"
-              fontFamily="Poppins-Bold"
-              fontSize={17}
-              padding={0}
-              borderRadius={0}
-              alignSelf="flex-end"
-              onPress={() => Alert.alert('testing')}
-            />
-            <Text style={styles.signText}>Não tem conta? </Text>
-            <Button
-              text="Cadastre-se"
-              color="#8E3385"
-              width={125}
-              height={27}
-              colorText="#FFFFFF"
-              fontFamily="Poppins-Bold"
-              fontSize={17}
-              padding={0}
-              borderRadius={6}
-              alignSelf="center"
-              onPress={() => Alert.alert('testing')}
-            />
+          <View style={styles.bodyContainer}>
+            <View style={styles.fieldContainer}>
+              <TextInput fieldName="E-mail" placeHolder="Digite seu e-mail" />
+              <TextInput fieldName="Senha" placeHolder="Digite sua senha" />
+            </View>
+            <View style={styles.linksContainer}>
+              <Button
+                text="Esqueceu a senha?"
+                color="transparent"
+                width={180}
+                height={26}
+                colorText="#FFFFFF"
+                fontFamily="Poppins-Bold"
+                fontSize={17}
+                padding={0}
+                borderRadius={0}
+                alignSelf="flex-end"
+                onPress={() => Alert.alert('testing')}
+              />
+              <Text style={styles.signText}>Não tem conta? </Text>
+              <Button
+                text="Cadastre-se"
+                color="#8E3385"
+                width={125}
+                height={27}
+                colorText="#FFFFFF"
+                fontFamily="Poppins-Bold"
+                fontSize={17}
+                padding={0}
+                borderRadius={6}
+                alignSelf="center"
+                onPress={() => Alert.alert('testing')}
+              />
+            </View>
+            <View style={styles.buttonContainer}>
+              <Button
+                text="Entrar"
+                color="#8E3385"
+                width={126}
+                height={48}
+                colorText="#FFFFFF"
+                fontFamily="Poppins-Regular"
+                fontSize={24}
+                padding={6}
+                borderRadius={30}
+                alignSelf="center"
+                onPress={() => Alert.alert('testing')}
+              />
+            </View>
           </View>
-          <View style={styles.buttonContainer}>
+          <View style={styles.bottomContainer}>
             <Button
-              text="Entrar"
-              color="#8E3385"
-              width={126}
-              height={48}
-              colorText="#FFFFFF"
-              fontFamily="Poppins-Regular"
-              fontSize={24}
-              padding={6}
+              text="Voltar"
+              color="#FFFFFF"
+              width={106}
+              height={42}
+              colorText="#8E3385"
+              fontFamily="Poppins-Bold"
+              fontSize={18}
+              padding={7}
               borderRadius={30}
               alignSelf="center"
               onPress={() => Alert.alert('testing')}
             />
           </View>
-        </View>
-        <View style={styles.bottomContainer}>
-          <Button
-            text="Voltar"
-            color="#FFFFFF"
-            width={106}
-            height={42}
-            colorText="#8E3385"
-            fontFamily="Poppins-Bold"
-            fontSize={18}
-            padding={7}
-            borderRadius={30}
-            alignSelf="center"
-            onPress={() => Alert.alert('testing')}
-          />
-        </View>
-      </ImageBackground>
+        </ImageBackground>
+      </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
   </View>
 
