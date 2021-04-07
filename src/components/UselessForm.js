cc14652b4439030e9a7c107f3a60b914499d87ef
Rodolfo Feitosa <rsfeitosa88@gmail.com>
2021-04-07 20:11:39 -0300

    implanting

---
+++ src/components/UselessForm.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { View, TextInput, Text, StyleSheet } from 'react-native'
+
+const UselessTextInput = (props) => {
+  
+  return (
+    <View>
+      <Text style={styles.fieldName}>{props.text}</Text>
+      <TextInput 
+        inlineImageLeft='message'
+        inlineImagePadding={200}
+        style={styles.field}
+        placeholder={props.placeholder}
+        maxLength={30}
+        fontFamily='Poppins-Regular'
+        fontSize={17}
+        />
+    </View>    
+    )
+}
+
+const styles = StyleSheet.create({
+  fieldName: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: 17,
+    color: '#FFFFFF',
+  },
+  field: {
+    borderBottomWidth: 1,
+    borderColor: '#FFFFFF',
+    lineHeight: 0.5
+  }
+})
+
+export default UselessTextInput
+
