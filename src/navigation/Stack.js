705d2895facae5a119d58d9411bb701976183848
Rodolfo Feitosa <rsfeitosa88@gmail.com>
2021-04-07 20:12:46 -0300

    Upgraded

--- src/navigation/Stack.js
+++ src/navigation/Stack.js
@@ -4,31 +4,37 @@ import Start from '../views/Start'
 import Login from '../views/Login'
 import PreLoad from '../views/PreLoad'
 import SignUp from '../views/SignUp'
+import RembPass from '../views/RembPass'
 
 const Stack = createStackNavigator()
 
 export default () => (
   
-  <Stack.Navigator initialRouteName="PreLoad">
+  <Stack.Navigator initialRouteName='RembPass'>
     <Stack.Screen
-      name="PreLoad"
-      options={{ headerShown: false }}
+      name='PreLoad'
+      options={{headerShown: false}}
       component={PreLoad}
     />
     <Stack.Screen
-      name="Start"
-      options={{ headerShown: false }}
+      name='Start'
+      options={{headerShown: false}}
       component={Start}
     />
     <Stack.Screen
-      name="Login"
-      options={{ headerShown: false }}
+      name='Login'
+      options={{headerShown: false}}
       component={Login}
     />
     <Stack.Screen
-      name="SignUp"
-      options={{ headerShown: false }}
+      name='SignUp'
+      options={{headerShown: false}}
       component={SignUp}
+    />
+    <Stack.Screen
+      name='RembPass'
+      options={{headerShown: false}}
+      component={RembPass}
     />
     
   </Stack.Navigator>
