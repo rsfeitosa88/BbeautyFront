ef2e4594a0feb8f6b11cc57cf65fb3e61a36e44f
Rodolfo Feitosa <rsfeitosa88@gmail.com>
2021-04-12 22:52:13 -0300

    signUp done

---
+++ src/views/SignUp_1.js
@@ -0,0 +1,175 @@
+import React from 'react'
+import { 
+    ImageBackground,
+    Text, 
+    View, 
+    KeyboardAvoidingView, 
+    Keyboard, 
+    TouchableWithoutFeedback, 
+    Platform,
+    Image } from 'react-native'
+
+import Button from '../components/ButtonFactory'
+import UselessForm from '../components/UselessForm'
+import signUp_1Styles from '../assets/styles/signUp_1Styles'
+
+export default SignUp_1 = props => {
+  
+  const [ nome, setNome ] = React.useState()
+  const [ sobrenome, setSobrenome ] = React.useState()
+  const [ endereco, setEndereco ] = React.useState()
+  const [ numero, setNumero ] = React.useState()
+  
+  const image = '../assets/images/esmaltes.jpg'
+  state = { isFocused: true }
+
+  return (
+
+    <View style={signUp_1Styles.container}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+        keyboardVerticalOffset={Platform.OS == 'ios' ? 0 : 20}
+        enabled={Platform.OS === 'ios' ? true : false}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <ImageBackground
+            source={require(image)}
+            style={signUp_1Styles.image}
+            blurRadius={50}
+          >
+            <View style={signUp_1Styles.headContainer}>
+              <Text 
+                style={[
+                signUp_1Styles.textBold, 
+                signUp_1Styles.firstTextHead
+                ]}
+              >
+                Seja bem-vinde
+              </Text>
+              <Text 
+                style={[
+                  signUp_1Styles.textNormal, 
+                  signUp_1Styles.firstTextHead, 
+                  signUp_1Styles.space
+                ]}
+              >
+                à Bbeauty
+              </Text>
+              <Text 
+                style={[
+                  signUp_1Styles.textNormal, 
+                  signUp_1Styles.scndTextHead
+                ]}
+              >
+                Nós e os profissionais, estamos 
+                ansiosos para mostrarmos os trabalhos. 
+                Mas antes, precisamos de alguns dados.
+              </Text>
+            </View>
+            <View style={signUp_1Styles.photoContainer}>
+              <Image 
+                source={{
+                  uri: 
+                    'https://cdn1.vectorstock.com/i/thumb-large/38/10/solid-purple-gradient-user-icon-web-icon-vector-23623810.jpg'
+                }}
+                style={signUp_1Styles.avatar}
+              />
+              <Button
+                text='Escolher foto'
+                color='#8E3385'
+                width={150}
+                height={40}
+                colorText='#FFFFFF'
+                fontFamily='Poppins-Regular'
+                fontSize={15}
+                borderRadius={10}
+                justifyContent='center'
+                alignSelf='center'
+              />
+            </View>
+            <View style={signUp_1Styles.inputContainer}>
+                <UselessForm
+                  text='Nome'
+                  maxLenght={15}
+                  fontFamily='Poppins-Regular'
+                  fontSize={17}
+                  width={170}
+                  value={nome}
+                  onChange={nome => setNome(nome) }
+                  returnKeyType='next'
+                  onSubmitEditing={() => {this.input_2.focus()}}
+                  blurOnSubmit={false}
+                />
+                <UselessForm
+                  text='Sobrenome'
+                  maxLenght={15}
+                  fontFamily='Poppins-Regular'
+                  fontSize={17}
+                  width={170}
+                  value={sobrenome}
+                  onChange={sobrenome => setSobrenome(sobrenome)}
+                  returnKeyType='next'
+                  blurOnSubmit={false}
+                  onSubmitEditing={() => (this.input_3.focus())}
+                  ref={input => this.input_2 = input}
+                />
+                <UselessForm
+                  text='Endereço'
+                  maxLenght={15}
+                  fontFamily='Poppins-Regular'
+                  fontSize={17}
+                  width={300}
+                  value={endereco}
+                  onChange={endereco => setEndereco(endereco)}
+                  blurOnSubmit={false}
+                  returnKeyType='next'
+                  onSubmitEditing={() => this.input_4.focus()}
+                  ref={input => this.input_3 = input}
+                />
+                <UselessForm
+                  text='Nº'
+                  maxLenght={15}
+                  fontFamily='Poppins-Regular'
+                  fontSize={17}
+                  width={50}
+                  value={numero}
+                  onChange={numero => setNumero(numero)}
+                  blurOnSubmit={false}
+                  returnKeyType='done'
+                  ref={input => this.input_4 = input}
+                />
+                <Button
+                  text='Voltar'
+                  color='#FFFFFF'
+                  width={106}
+                  height={42}
+                  colorText='#8E3385'
+                  fontFamily='Poppins-Bold'
+                  fontSize={19}
+                  padding={6}
+                  borderRadius={30}
+                  alignSelf='center'
+                  marginTop={30}
+                  onPress={() => props.navigation.navigate('Login')}
+                />
+                <Button
+                  text='Próximo Passo'
+                  color='#8E3385'
+                  width={152}
+                  height={42}
+                  colorText='#FFFFFF'
+                  fontFamily='Poppins-Bold'
+                  fontSize={18.5}
+                  padding={6}
+                  borderRadius={30}
+                  alignSelf='center'
+                  marginTop={30}
+                  onPress={() => props.navigation.navigate('SignUp_2')}
+                />
+            </View>
+          </ImageBackground>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    </View>
+  )
+}
---
+++ src/views/SignUp_2.js
@@ -0,0 +1,147 @@
+import React from 'react'
+import {
+    ImageBackground,
+    Text,
+    View,
+    KeyboardAvoidingView,
+    Keyboard,
+    TouchableWithoutFeedback,
+    Platform,
+    StyleSheet
+} from 'react-native'
+
+import ButtonFactory from '../components/ButtonFactory'
+import UselessForm from '../components/UselessForm'
+import signUp_2Styles from '../assets/styles/signUp_2Styles'
+import SignUp_1 from '../views/SignUp_1'
+
+export default SignUp_2 = props => {
+
+  const image = '../assets/images/esmaltes.jpg'
+  state = { isFocused: true }
+
+  const [ email, setEmail ] = React.useState()
+  const [ password, setPassword ] = React.useState()
+  const [ rPassword, setRpassword ] = React.useState()
+
+  return (
+    <View style={signUp_2Styles.container}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+        keyboardVerticalOffset={Platform.OS == 'ios' ? 0 : 20}
+        enabled={Platform.OS === 'ios' ? true : false}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <ImageBackground
+            source={require(image)}
+            style={signUp_2Styles.image}
+            blurRadius={50}
+          >
+            <View style={signUp_2Styles.headContainer}>
+              <Text 
+                style={
+                  [signUp_2Styles.textBold, 
+                  signUp_2Styles.firstTextHead]
+                }
+                >Falta Pouco,{'\n'}(nome user).
+              </Text>
+              <Text 
+                style={[
+                  signUp_2Styles.firstTextHead,
+                  signUp_2Styles.textNormal,
+                  signUp_2Styles.space 
+                ]}
+                  >Só alguns dados e pronto!
+              </Text>
+              <Text 
+                style={[
+                  signUp_2Styles.scndTextHead,
+                  signUp_2Styles.textNormal
+                ]}>
+                Após a criação do perfil, 
+                enviaremos um email com link, 
+                para confirmar o seu cadastro e 
+                ter acesso ao App
+              </Text>
+            </View>
+            <View style={signUp_2Styles.inputContainer}>
+              <UselessForm
+                text='E-mail'
+                maxLenght={15}
+                fontFamily='Poppins-Regular'
+                fontSize={17}
+                width={'auto'}
+                value={email}
+                onChange={email => setEmail(email)}
+                returnKeyType='next'
+                onSubmitEditing={() => {this.input_2.focus()}}
+                blurOnSubmit={false}
+              />
+              <Text style={signUp_2Styles.passText}>
+                A senha deve ter no mínimo:{'\n'}6 digitos, 1 número e 1 caractere especial
+              </Text>
+              <UselessForm
+                text='Senha'
+                maxLenght={15}
+                fontFamily='Poppins-Regular'
+                fontSize={17}
+                width={'auto'}
+                secureTextEntry={true}
+                value={password}
+                onChange={password => setPassword(password)}
+                returnKeyType='next'
+                blurOnSubmit={false}
+                onSubmitEditing={() => (this.input_3.focus())}
+                ref={input => this.input_2 = input}
+              />
+              <UselessForm
+                text='Confirme a Senha'
+                maxLenght={15}
+                fontFamily='Poppins-Regular'
+                fontSize={17}
+                width={'auto'}
+                secureTextEntry={true}
+                value={rPassword}
+                onChange={rPassword => setRpassword(rPassword)}
+                blurOnSubmit={false}
+                returnKeyType='done'
+                ref={input => this.input_3 = input}
+                />
+            </View>
+            <View style={signUp_2Styles.bottomContainer}>
+              <ButtonFactory
+              text='Voltar'
+              color='#FFFFFF'
+              width={106}
+              height={42}
+              colorText='#8E3385'
+              fontFamily='Poppins-Bold'
+              fontSize={19}
+              padding={6}
+              borderRadius={30}
+              alignSelf='center'
+              marginTop={30}
+              onPress={() => props.navigation.navigate('Login')}
+             />
+            <ButtonFactory
+              text='Próximo Passo'
+              color='#8E3385'
+              width={152}
+              height={42}
+              colorText='#FFFFFF'
+              fontFamily='Poppins-Bold'
+              fontSize={18.5}
+              padding={6}
+              borderRadius={30}
+              alignSelf='center'
+              marginTop={30}
+              onPress={() => props.navigation.navigate('SignUp_2')}
+            />
+            </View>
+          </ImageBackground>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+          
+      </View>
+  )
+}
